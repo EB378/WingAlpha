6966bc6ca42b1fddd6c8f1319057182a2e4c5192
EAlexBen <107051871+EB378@users.noreply.github.com>
2025-01-04 23:47:56 +0200

    Update page.tsx

--- app/[locale]/protected/page.tsx
+++ app/[locale]/protected/page.tsx
@@ -34,10 +34,10 @@ export default async function ProtectedPage({
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <Link href={`/${locale}/protected/alpha/gcal`}>
-          <h2 className="font-bold text-2xl mb-4">Alpha</h2>
+          <h2 className="font-bold text-2xl mb-4">GCal</h2>
         </Link>
         <Link href={`/${locale}/protected/alpha`}>
-          <h2 className="font-bold text-2xl mb-4">Grace, Click Here!</h2>
+          <h2 className="font-bold text-2xl mb-4">Alpha</h2>
         </Link>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           {JSON.stringify(user, null, 2)}
